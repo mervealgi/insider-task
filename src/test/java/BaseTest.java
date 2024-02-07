8a195d2de1bb97a879ee4853bd2b2b5b1770ca0c
merve <mervealgi@gmail.com>
2024-02-07 23:26:46 +0300

    init

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,21 +2,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BaseTest {
-    WebDriver chromeDriver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
 
     @BeforeTest
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
-        chromeDriver.get("https://useinsider.com/");
-
+        driver.get("https://useinsider.com/");
     }
 
     @AfterTest
     public void tearDown(){
-
-        chromeDriver.quit();
+        driver.quit();
     }
 }
